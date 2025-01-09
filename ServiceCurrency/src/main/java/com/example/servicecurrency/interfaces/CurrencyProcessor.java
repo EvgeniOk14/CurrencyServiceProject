43b070edf01cdb374c7fd34fbca7c22f06a1ac20
Evgeni.ok14 <evgeni.ok@mail.ru>
2025-01-09 14:25:39 +0300

    Project CurrencyService

--- ServiceCurrency/src/main/java/com/example/servicecurrency/interfaces/CurrencyProcessor.java
+++
@@ -1,8 +0,0 @@
-package com.example.servicecurrency.interfaces;
-
-import com.example.servicecurrency.models.abstructModels.Currency;
-
-public interface CurrencyProcessor
-{
-    Currency getCurrency(String code);
-}
